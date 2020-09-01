0f8495794b63cd372a25159b77b4384a53d363f0
tayyabroy786 <30634558+tayyabroy786@users.noreply.github.com>
2020-09-01 23:36:11 +0500

    some new changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Grid } from '@material-ui/core';
 
-import { Cards, LineChart, PieChart, CountryPicker, Header } from './components';
+import { Cards, LineChart, PieChart, CountryPicker, Header, } from './components';
 import { GlobalContext } from './context/GlobalState';
 
 import styles from './App.module.css';
@@ -12,17 +12,14 @@ function App() {
 	return (
 		<div className={styles.container}>
 			<Grid container spacing={3} direction="row" justify="space-around" alignItems="center">
-
 				<Grid item xs={12} md={12} zeroMinWidth>
 					<Header />
 					<CountryPicker />
 				</Grid>
-
-				<Grid item xs={12} md={12} zeroMinWidth>
+				<Grid item xs={4} md={4} zeroMinWidth>
 					<Cards />
 				</Grid>
-
-				<Grid item xs={12} md={12} zeroMinWidth>
+				<Grid item xs={7} md={7} zeroMinWidth>
 					{country ? <PieChart /> : <LineChart />}
 				</Grid>
 			</Grid>
